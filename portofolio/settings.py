eae449b4e25ccbf70551e96ae89f0f5b076e81db
Nadhif Aydin Adinandra <tayanadhif@gmail.com>
2026-09-02 16:45:32 +0700

    Fix static files deployment

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -149,6 +149,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+WHITENOISE_USE_FINDERS = True
 
 STORAGES = {
     'default': {

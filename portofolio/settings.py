62daebcbf67015b018b04000c2ee3fbbffc60666
Nadhif Aydin Adinandra <tayanadhif@gmail.com>
2026-09-02 11:38:25 +0700

    Fix static files deployment

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -155,7 +155,7 @@ STORAGES = {
         'BACKEND': 'django.core.files.storage.FileSystemStorage',
     },
     'staticfiles': {
-        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+        'BACKEND': 'whitenoise.storage.CompressedStaticFilesStorage',
     },
 }
 

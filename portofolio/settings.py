c376ceb8140df33c030908ebf2a439da76a72b05
Nadhif Aydin Adinandra <tayanadhif@gmail.com>
2026-09-05 12:15:32 +0700

    Mengetes send email apakah sudah masuk

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -178,6 +178,8 @@ EMAIL_BACKEND = os.getenv(
 EMAIL_HOST = os.getenv('EMAIL_HOST', '')
 EMAIL_PORT = int(os.getenv('EMAIL_PORT', '587'))
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True').lower() == 'true'
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', 'False').lower() == 'true'
+EMAIL_TIMEOUT = int(os.getenv('EMAIL_TIMEOUT', '15'))
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '').strip()
 EMAIL_HOST_PASSWORD = ''.join(os.getenv('EMAIL_HOST_PASSWORD', '').split())
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'noreply@localhost')
